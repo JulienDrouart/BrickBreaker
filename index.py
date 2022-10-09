3df1fa7e12ff524e69059e0881f740a68185f612
Julien Drouart <julien.drouart.pro@gmail.com>
2022-10-09 19:17:12 +0200

    Ajout de la gravité de la balle et détection si touché

--- index.py
+++ index.py
@@ -7,6 +7,7 @@ import sqlite3
 from pygame.locals import *
 from sqlite3 import Error
 from collections import OrderedDict
+import pyautogui
 
 connection = sqlite3.connect('score.db')
 cursor = connection.cursor()
@@ -44,12 +45,21 @@ def loopFunction():
     start = gameover = False
     fond = pygame.image.load("fond.jpg")
     screen.blit(fond, (0, 0))
-    brickCooX = 500
-    brick = Rect(brickCooX, 750, 100, 10)
+    brickCooX = 450
+
+    coeur = pygame.image.load("coeur.png")
+
+    brickColor = (randint(0, 255), randint(0, 255), (randint(0, 255)))
+    pygame.draw.rect(screen, brickColor, Rect(brickCooX, 750, 100, 10))
     brickSpeed = 9
     direction = "null"
     font = pygame.font.Font('freesansbold.ttf', 32)
-
+    ballPosX = 500
+    ballPosY = 500
+    ballXMove = 0
+    ballYMove = 3
+    ballColor = (randint(0, 255), randint(0, 255), randint(0, 255))
+    pygame.draw.circle(screen, (randint(0, 255), randint(0, 255), randint(0, 255)), (ballPosX, ballPosY), 10)
     highscore = selectDatabase()
     highestScore = font.render('Highest Score : ' + str(highscore), True, (255, 255, 255))
     screen.blit(highestScore, (700, 20))
@@ -60,13 +70,11 @@ def loopFunction():
     intervalOf10 = row = 0
     bricks = {}
     bricksNumber = 70
-    pygame.draw.rect(screen, (randint(0, 255), randint(0, 255), (randint(0, 255))), brick)
-
 
     for i in range(bricksNumber):
-        bricks[i] = {'posX': (intervalOf10*100), 'posY': (row * 50) + 100, 'state': "alive"}
-        pygame.draw.rect(screen, (randint(0, 255), randint(0, 255), randint(0, 255)),
-                         Rect(bricks[i]["posX"], bricks[i]["posY"], 100, 50))
+        color = (randint(0, 255), randint(0, 255), randint(0, 255))
+        bricks[i] = {'posX': (intervalOf10 * 100), 'posY': (row * 50) + 100, 'state': "alive", "color": color}
+        pygame.draw.rect(screen, color, Rect(bricks[i]["posX"], bricks[i]["posY"], 100, 50))
         if intervalOf10 < 9:
             intervalOf10 += 1
         else:
@@ -107,27 +115,44 @@ def loopFunction():
                 if brickCooX < 10:
                     brickCooX = brickCooX
                 else:
-                    brickCooX = brickCooX - brickSpeed
+                    brickCooX -= brickSpeed
             if direction == "right":
                 if brickCooX > 940:
                     brickCooX = brickCooX
                 else:
-                    brickCooX = brickCooX + brickSpeed
+                    brickCooX += brickSpeed
 
             if start is True:
                 screen.blit(fond, (0, 0))
+
+                screen.blit(coeur, (10, 5))
+                screen.blit(coeur, (70, 5))
+                screen.blit(coeur, (130, 5))
+
                 currentScoreText = font.render('Score : ' + str(score), True, (255, 255, 255))
                 scoreText = font.render('Highest Score : ' + str(highscore), True, (255, 255, 255))
-                screen.blit(currentScoreText, (200, 20))
+                screen.blit(currentScoreText, (400, 20))
                 screen.blit(scoreText, (700, 20))
                 atLeastOneAlive = False
+                brick = Rect(brickCooX, 750, 100, 10)
+                ballCircle = Rect(ballPosX, ballPosY, 10, 10)
+                pygame.draw.rect(screen, brickColor, brick)
+                pygame.draw.circle(screen, ballColor, (ballPosX, ballPosY), 10)
+                ballPosY += ballYMove
+                ballPosX += ballXMove
 
-                for i in range(bricksNumber):
-                    print("ici")
 
-                if not atLeastOneAlive:
-                    gameover = True
+                for i in bricks:
+                    pygame.draw.rect(screen, bricks[i]["color"], Rect(bricks[i]["posX"], bricks[i]["posY"], 100, 50))
+                if 750 < ballPosY < 755 and pygame.Rect.colliderect(brick, ballCircle) == False:
+                    print("pas touché")
+                # if not atLeastOneAlive:
+                # gameover = True
 
+                ballPosYText = font.render(str(ballPosY), True, (255, 255, 255))
+                screen.blit(ballPosYText, (600, 20))
+
+        #print(pyautogui.position())
         clock.tick(60)
         pygame.display.flip()
 
